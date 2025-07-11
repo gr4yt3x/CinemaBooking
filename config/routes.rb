ae9d50b187b149df609c1af5ecee354e9fea00da
lemos <lemos@ubntu.myguest.virtualbox.org>
2025-07-11 05:00:32 -0300

    Add cities entity with migration and controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  root 'home#index' 
+  root "home#index"
 
+  # City
+  resources :cities, except: [ :new, :edit ]
 end
